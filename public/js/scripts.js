bd6e95639e6151570118d6142a5c9b3cb9ce3a28
Jerson Miranda <56894248+nullbeam@users.noreply.github.com>
2024-10-22 11:57:19 -0500

    Revert "feat: added faq section"

--- public/js/scripts.js
+++
@@ -1,8 +0,0 @@
-document.querySelectorAll(".faq-card").forEach(function(div) {
-    div.addEventListener("click", function() {
-        document.querySelectorAll(".faq-card").forEach(function(el) {
-            el.classList.remove("active");
-        });
-        this.classList.toggle("active");
-    });
-});
